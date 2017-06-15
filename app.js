b26a96cc246431e9d64ac163b1c9371a595b0a25
Samseppiol <j_hage@live.com>
2017-06-15 11:34:22 +1000

    Added scripts

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+// Create Express app
+const app = express()
+
+//  Start the server
+app.listen(7000, () => {
+  console.log('Server Started on port 7000')
+})
